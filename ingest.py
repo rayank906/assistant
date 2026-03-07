fab94bcf87158eecf0bf93281e2d467c31f2481f
Rayan Kamdem <rayankdm@umich.edu>
2026-03-07 00:09:21 -0500

    added text extraction + chunking

---
+++ ingest.py
@@ -0,0 +1,21 @@
+import fitz
+import numpy as np
+import faiss
+
+PDF_PATH = "eecs280notes.pdf"
+
+def extract_text_from_pdf(pdf_path):
+    doc = fitz.open(pdf_path)
+    text = ""
+    for page in doc:
+        text += page.get_text()
+    return text
+
+def chunk_text(text, chunk_size=500, overlap=100):
+    chunks = []
+    start = 0
+    while start < len(text):
+        end = start + chunk_size
+        chunks.append(text[start:end])
+        start = end - overlap
+    return chunks
